c4c355b353d61734632a01e56f74c22e29d243ce
Martyna Łęcka <martynalecka.ml@gmail.com>
2019-01-18 22:49:11 +0100

    Worked on class Student

--- include/Student.hpp
+++ include/Student.hpp
@@ -1,8 +1,17 @@
+#include <string>
 
 class Student
 {
-    public:
-
-    private:
-
+public:
+    Student() = delete;
+    Student(unsigned int id, string firstN, string lastN):
+        index(id),firstName(firstN),lastName(lastN){}; 
+    ~Student();
+    unsigned int getIndexNumber() const;
+    string getFirstName() const;
+    string getLastName() const;
+private:
+    unsigned int index;
+    string firstName;
+    string lastName;
 };
--- src/Student.cpp
+++ src/Student.cpp
@@ -1,2 +1,14 @@
 #include "../include/Student.hpp"
 
+unsigned int Student::getIndexNumber()
+{
+    return index;
+}
+string Student::getFirstName()
+{
+    return firstName;
+}
+string Student::getLastName()
+{
+    return lastName;
+}
