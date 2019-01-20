6913a490da47acdb2566c215e9a9e5bb132a0eb1
OriaTori <37372074+OriaTori@users.noreply.github.com>
2019-01-20 01:53:01 +0100

    Martyna (#1)
    Fixed CMakeLists.txt file: problem with linking cpp file: added aux_source_directory() function
    Done task: add a method which allows added student to the base.
    Added function showing all students in the base.

--- include/Student.hpp
+++ include/Student.hpp
@@ -1,8 +1,19 @@
+#pragma once
+#include <string>
 
 class Student
 {
-    public:
-
-    private:
-
+public:
+    Student() = delete;
+    Student(unsigned int id, std::string firstN, std::string lastN):
+        index(id),firstName(firstN),lastName(lastN){}; 
+    Student(const Student& student); 
+    ~Student() = default;
+    unsigned int getIndexNumber() const;
+    std::string getFirstName() const;
+    std::string getLastName() const;
+private:
+    unsigned int index;
+    std::string firstName;
+    std::string lastName;
 };
--- src/StudentsBase.cpp
+++ src/StudentsBase.cpp
@@ -1,2 +1,30 @@
-#include "../include/StudentsBase"
+#include "../include/StudentsBase.hpp"
+#include <iostream>
+
+void StudentsBase::showBase()
+{
+    if(studentBase.empty() != true)
+    {
+        for(const Student& student : studentBase)
+        {
+            std::cout<< " First name: "<< student.getFirstName();
+            std::cout<< " Last name: "<< student.getLastName();
+            std::cout<< " Index: "<< student.getIndexNumber() << std::endl;
+        }
+    }else
+        std::cout<< "Empty students base" << std::endl;
+}
+void StudentsBase::addStudent(Student* student)
+{
+    studentBase.push_back(*student);
+}
+void StudentsBase::sortByIndexAsc(bool variant)
+{
+
+}
+void StudentsBase::removeStudent(int index)
+{
+
+}
+
 
