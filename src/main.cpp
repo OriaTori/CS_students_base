40c1a359d4e37556cca4e5c706b0d84809c5e925
Martyna Łęcka <martynalecka.ml@gmail.com>
2019-03-25 22:54:01 +0100

    The unnecessary std::move has been removed.

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main()
     Student* student = new Student(200118,"Jan","Kowalski");
     std::unique_ptr<Student> stud(new Student(200111,"Piotr","Nowak"));
     base.addStudent(std::move(stud));
-    base.addStudent(std::move(std::make_unique<Student>(*student)));
+    base.addStudent(std::make_unique<Student>(*student));
     base.showBase();
     delete student;
     return 0;
